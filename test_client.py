80c9f0d2fa9b3d92af02cb1ab7caddc2b8232670
Kosmonautt <b.contreras.2@ug.uchile.cl>
2023-10-17 20:35:53 -0300

    test 1 and 2 pass but test 3 gets stuck

--- test_client.py
+++ test_client.py
@@ -7,6 +7,12 @@ client_socketTCP = SocketTCP.SocketTCP()
 client_socketTCP.set_socketUDP(socket.socket(socket.AF_INET, socket.SOCK_DGRAM))
 client_socketTCP.connect(address)
 
-# test 1 
+# test 1
 message = "Mensje de len=16".encode()
+client_socketTCP.send(message)
+# test 2
+message = "Mensaje de largo 19".encode()
+client_socketTCP.send(message)
+# test 3
+message = "Mensaje de largo 19".encode()
 client_socketTCP.send(message)
